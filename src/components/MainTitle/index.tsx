66a25a0ffa5020ec08bcaa4455651f32b8978427
cavez86 <cavez86@gmail.com>
2026-01-15 01:02:53 +0100

    feat(mainTitle): add MainTitle component with styles

---
+++ src/components/MainTitle/index.tsx
@@ -0,0 +1,11 @@
+import classes from './styles.module.css';
+
+const MainTitle = () => {
+  return (
+    <h1 className={classes.title}>
+      <span className={classes.firstWord}>User</span> Dashboard
+    </h1>
+  );
+};
+
+export default MainTitle;
