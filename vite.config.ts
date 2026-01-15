b30adf5d2bae6d180780dad8908dde08c67c9afe
cavez86 <cavez86@gmail.com>
2026-01-15 01:02:11 +0100

    feat(config): update CSS modules configuration to use camelCase localsConvention

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
   test: {
-    include: ["src/**/*.test.{js,ts,tsx}"],
+    include: ['src/**/*.test.{js,ts,tsx}'],
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
   },
 });
