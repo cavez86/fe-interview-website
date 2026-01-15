ab56226948c85717f9c118d5cb238c8cdf46be71
cavez86 <cavez86@gmail.com>
2026-01-16 00:02:45 +0100

    feat(searchbar): refactor SearchBar component to handle form submission and improve input handling

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -3,17 +3,23 @@ import Button from '../Button';
 import classes from './styles.module.css';
 
 const SearchBar = () => {
-  const formId = useId();
+  const inputId = useId();
+
+  const handleSubmit = (formData: FormData) => {
+    const query = formData.get('search');
+    console.log(`Search query: ${query}`);
+  };
+
   return (
-    <div className={classes.container}>
-      <label className={classes.label} htmlFor={formId}>
+    <form className={classes.container} action={handleSubmit}>
+      <label className={classes.label} htmlFor={inputId}>
         what are you looking for?
       </label>
       <div className={classes.searchBar}>
-        <input id={formId} className={classes.input} type='text' placeholder='Search by name...' />
-        <Button className={classes.button} label='Search' />
+        <input id={inputId} className={classes.input} name='search' type='text' placeholder='Search by name...' />
+        <Button className={classes.button} label='Search' type='submit' />
       </div>
-    </div>
+    </form>
   );
 };
 
