887f2825871c6645e8ee2c4343950f3db5c69cd5
cavez86 <cavez86@gmail.com>
2026-01-15 01:11:41 +0100

    feat(header-section): update header section styles and structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SearchBar from './components/SearchBar';
 const App = () => {
   return (
     <main className='main-content'>
-      <section>
+      <section className='header-section'>
         <MainTitle />
         <SearchBar />
       </section>
