050a94a2af82d4235c893424aef5879696f1c526
cavez86 <cavez86@gmail.com>
2026-01-16 01:46:57 +0100

    feat(UsersGrid): add no results message and clear filters button

--- src/components/UsersGrid/index.tsx
+++ src/components/UsersGrid/index.tsx
@@ -1,10 +1,30 @@
+import { useFilters } from '../../hooks/useFilters';
 import { useUsers } from '../../hooks/useUsers';
+import Button from '../Button';
 import UserCard from '../UserCard';
 
 import classes from './styles.module.css';
 
 const UsersGrid = () => {
+  const { search, role, setSearch, setRoleFilter } = useFilters();
   const users = useUsers();
+
+  const resetFilters = () => {
+    setSearch(null);
+    setRoleFilter(null);
+  };
+
+  if (search?.length && !users.length) {
+    return (
+      <div className={classes.noResults}>
+        <p>
+          No users found for "{search}" {role && `with role "${role}"`}
+        </p>
+        <Button label='Clear filters' className={classes.button} onClick={resetFilters} />
+      </div>
+    );
+  }
+
   return (
     <div className={classes.grid}>
       {users.map((user) => (
