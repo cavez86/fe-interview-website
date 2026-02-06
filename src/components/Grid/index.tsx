8827a92fc41fb5da09ab40f8b3d1bdfeafabcf0f
cavez86 <cavez86@gmail.com>
2026-02-06 14:52:51 +0100

    feat: add pagination to user grid with updated styles

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,10 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { filterUsers } from "../../data/users";
 import { useFilters } from "../../hooks/useFilters";
 import { useUsers } from "../../hooks/useUsers";
 import Button from "../Button";
 import Card from "../Card";
+import { Pagination } from "../Pagination";
 import classes from "./styles.module.css";
+
+const ITEMS_PER_PAGE = 8;
 
 const Grid = () => {
   const { search, role, setSearch, setRoleFilter } = useFilters();
@@ -16,6 +19,26 @@ const Grid = () => {
   };
 
   const filteredUsers = useMemo(() => filterUsers(users, role), [users, role]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(filteredUsers.length / ITEMS_PER_PAGE);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev));
+  };
+
+  const paginatedUsers = useMemo(() => {
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    return filteredUsers.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+  }, [filteredUsers, currentPage]);
 
   if (loading) {
     return <p className={classes.message}>Loading users...</p>;
@@ -42,10 +65,21 @@ const Grid = () => {
   }
 
   return (
-    <div className={classes.grid}>
-      {filteredUsers.map((user) => (
-        <Card key={user.id} user={user} />
-      ))}
+    <div className={classes.gridContainer}>
+      <div className={classes.grid}>
+        {paginatedUsers.map((user) => (
+          <Card key={user.id} user={user} />
+        ))}
+      </div>
+      {!!paginatedUsers.length && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          onPrevious={handlePrevious}
+          onNext={handleNext}
+        />
+      )}
     </div>
   );
 };
